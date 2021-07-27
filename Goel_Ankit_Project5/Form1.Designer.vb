2e0c9fff78e25aeef3ce47d154021c942841d8c4
Ankit Goel <59352341+ankitgoel2001@users.noreply.github.com>
2021-07-26 17:35:15 -0700

    Add files via upload

---
+++ Goel_Ankit_Project5/Form1.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtBoxName = New System.Windows.Forms.TextBox()
+        Me.txtBoxSeat = New System.Windows.Forms.TextBox()
+        Me.lblSeat = New System.Windows.Forms.Label()
+        Me.btnAddPassenger = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.btnResetAll = New System.Windows.Forms.Button()
+        Me.btnListPassenger = New System.Windows.Forms.Button()
+        Me.btnDeletePassenger = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblPassengerCount = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lstBoxWaitlist = New System.Windows.Forms.ListBox()
+        Me.VScrollBar1 = New System.Windows.Forms.VScrollBar()
+        Me.lstSeatChart = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Name:"
+        '
+        'txtBoxName
+        '
+        Me.txtBoxName.Location = New System.Drawing.Point(103, 36)
+        Me.txtBoxName.Name = "txtBoxName"
+        Me.txtBoxName.Size = New System.Drawing.Size(156, 22)
+        Me.txtBoxName.TabIndex = 1
+        '
+        'txtBoxSeat
+        '
+        Me.txtBoxSeat.Location = New System.Drawing.Point(373, 39)
+        Me.txtBoxSeat.Name = "txtBoxSeat"
+        Me.txtBoxSeat.Size = New System.Drawing.Size(156, 22)
+        Me.txtBoxSeat.TabIndex = 3
+        '
+        'lblSeat
+        '
+        Me.lblSeat.AutoSize = True
+        Me.lblSeat.Location = New System.Drawing.Point(326, 39)
+        Me.lblSeat.Name = "lblSeat"
+        Me.lblSeat.Size = New System.Drawing.Size(41, 17)
+        Me.lblSeat.TabIndex = 2
+        Me.lblSeat.Text = "&Seat:"
+        '
+        'btnAddPassenger
+        '
+        Me.btnAddPassenger.Location = New System.Drawing.Point(48, 111)
+        Me.btnAddPassenger.Name = "btnAddPassenger"
+        Me.btnAddPassenger.Size = New System.Drawing.Size(195, 55)
+        Me.btnAddPassenger.TabIndex = 4
+        Me.btnAddPassenger.Text = "&Add Passenger"
+        Me.btnAddPassenger.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(48, 403)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(195, 55)
+        Me.btnClose.TabIndex = 5
+        Me.btnClose.Text = "&Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'btnResetAll
+        '
+        Me.btnResetAll.Location = New System.Drawing.Point(48, 331)
+        Me.btnResetAll.Name = "btnResetAll"
+        Me.btnResetAll.Size = New System.Drawing.Size(195, 53)
+        Me.btnResetAll.TabIndex = 6
+        Me.btnResetAll.Text = "&Reset All"
+        Me.btnResetAll.UseVisualStyleBackColor = True
+        '
+        'btnListPassenger
+        '
+        Me.btnListPassenger.Location = New System.Drawing.Point(48, 265)
+        Me.btnListPassenger.Name = "btnListPassenger"
+        Me.btnListPassenger.Size = New System.Drawing.Size(195, 49)
+        Me.btnListPassenger.TabIndex = 7
+        Me.btnListPassenger.Text = "&List Passenger"
+        Me.btnListPassenger.UseVisualStyleBackColor = True
+        '
+        'btnDeletePassenger
+        '
+        Me.btnDeletePassenger.Location = New System.Drawing.Point(48, 191)
+        Me.btnDeletePassenger.Name = "btnDeletePassenger"
+        Me.btnDeletePassenger.Size = New System.Drawing.Size(195, 49)
+        Me.btnDeletePassenger.TabIndex = 8
+        Me.btnDeletePassenger.Text = "&Delete Passenger"
+        Me.btnDeletePassenger.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(337, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(236, 17)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "(1A,1B,1C,1D===10A,10B,10C,10D)"
+        '
+        'lblPassengerCount
+        '
+        Me.lblPassengerCount.AutoSize = True
+        Me.lblPassengerCount.Location = New System.Drawing.Point(345, 308)
+        Me.lblPassengerCount.Name = "lblPassengerCount"
+        Me.lblPassengerCount.Size = New System.Drawing.Size(133, 17)
+        Me.lblPassengerCount.TabIndex = 11
+        Me.lblPassengerCount.Text = "Passenger Count: 0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(345, 340)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 17)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Wait List:"
+        '
+        'lstBoxWaitlist
+        '
+        Me.lstBoxWaitlist.FormattingEnabled = True
+        Me.lstBoxWaitlist.ItemHeight = 16
+        Me.lstBoxWaitlist.Location = New System.Drawing.Point(350, 371)
+        Me.lstBoxWaitlist.Name = "lstBoxWaitlist"
+        Me.lstBoxWaitlist.Size = New System.Drawing.Size(236, 84)
+        Me.lstBoxWaitlist.TabIndex = 13
+        '
+        'VScrollBar1
+        '
+        Me.VScrollBar1.Location = New System.Drawing.Point(571, 374)
+        Me.VScrollBar1.Name = "VScrollBar1"
+        Me.VScrollBar1.Size = New System.Drawing.Size(14, 83)
+        Me.VScrollBar1.TabIndex = 14
+        '
+        'lstSeatChart
+        '
+        Me.lstSeatChart.FormattingEnabled = True
+        Me.lstSeatChart.ItemHeight = 16
+        Me.lstSeatChart.Items.AddRange(New Object() {"Row       A     B     C     D", "    1", "    2", "    3", "    4", "    5", "    6", "    7", "    8", "    9", "  10"})
+        Me.lstSeatChart.Location = New System.Drawing.Point(340, 111)
+        Me.lstSeatChart.Name = "lstSeatChart"
+        Me.lstSeatChart.Size = New System.Drawing.Size(233, 180)
+        Me.lstSeatChart.TabIndex = 10
+        Me.lstSeatChart.Tag = "g"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(663, 470)
+        Me.Controls.Add(Me.VScrollBar1)
+        Me.Controls.Add(Me.lstBoxWaitlist)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblPassengerCount)
+        Me.Controls.Add(Me.lstSeatChart)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnDeletePassenger)
+        Me.Controls.Add(Me.btnListPassenger)
+        Me.Controls.Add(Me.btnResetAll)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnAddPassenger)
+        Me.Controls.Add(Me.txtBoxSeat)
+        Me.Controls.Add(Me.lblSeat)
+        Me.Controls.Add(Me.txtBoxName)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Project5"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtBoxName As TextBox
+    Friend WithEvents txtBoxSeat As TextBox
+    Friend WithEvents lblSeat As Label
+    Friend WithEvents btnAddPassenger As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnResetAll As Button
+    Friend WithEvents btnListPassenger As Button
+    Friend WithEvents btnDeletePassenger As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblPassengerCount As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lstBoxWaitlist As ListBox
+    Friend WithEvents VScrollBar1 As VScrollBar
+    Friend WithEvents lstSeatChart As ListBox
+End Class
---
+++ Goel_Ankit_Project5/Form1.vb
@@ -0,0 +1,270 @@
+﻿'******************************************************************************************
+'Name: Ankit Goel
+'Date: 6/19/2021
+'Purpose: Program to create an airline reservation booking system using 2d arrays
+'******************************************************************************************
+Public Class Form1
+    Dim aryintPassengers(11, 5) As String 'array of passenger seats with 10 rows and 4 columns
+    Dim aryPassengerName(11, 5) As String 'array to store passenger's name
+    Dim aryWaitlistName As ArrayList = New ArrayList() 'array to store the waitlist of people
+    Dim passengerCount As Integer = 0   'to store how many passengers are there
+
+    Private Sub formLoad(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Function to automatically fill the seat chart as specified in the bash script
+        Dim seatCount As String 'to store number of seats by user
+        Dim counter As Integer = 1 'to keep track of number of seats
+        seatCount = Command()
+
+        'To make sure that the user doesn't leave the seat count
+        If seatCount = "" Then
+            Exit Sub
+        End If
+
+        'To keep adding seats until counter
+        For i = 1 To 10
+            For j = 1 To 4
+                If counter > CInt(seatCount) Then
+                    lstSeatChart.Items.Clear()
+                    Call createArray()
+                    lblPassengerCount.Text = "Passenger count: " + CStr(countPassengers())
+                    Exit Sub
+                End If
+                aryintPassengers(i, j) = "X"
+                aryPassengerName(i, j) = "Pass".ToUpper & counter
+                counter += 1
+            Next
+        Next
+        lstSeatChart.Items.Clear()
+        Call createArray()
+        lblPassengerCount.Text = "Passenger count: " + CStr(countPassengers())
+
+    End Sub
+    Private Sub addPassenger()
+        'Sub function to add new passengers to the seats
+        Dim SeatNo As String 'to store the seat number
+        Dim first_index As Integer   'to store the number from the seat to get the seat row
+        Dim second_index As String    'to store the letter from the seat to get the seat column
+        Dim column As Integer         'to store the seat column
+        Dim isEmpty As Boolean = False 'to store as a flag to check if seat was empty
+        Dim passengerCount As Integer  'to keep count of number of passengers getting a seat
+
+        passengerCount = Val(lblPassengerCount.Text)
+        SeatNo = txtBoxSeat.Text.ToUpper   'to get the seat number from the text box
+        'to check if passenger didn't choose a seat
+        If SeatNo = "" Then
+            For first_index = 1 To 10
+                For column = 1 To 4
+                    'Assign them the next available empty seat
+                    If aryintPassengers(first_index, column) <> "X" Then
+                        aryintPassengers(first_index, column) = "X"
+                        aryPassengerName(first_index, column) = txtBoxName.Text.ToUpper
+                        lblPassengerCount.Text = "Passenger count: " + CStr(countPassengers())
+                        isEmpty = True
+                        Exit Sub
+                    End If
+                Next
+            Next
+            'To add passenger to the waitlist if no seat available
+            If isEmpty = False Then
+                Call addWaitlist()
+                Exit Sub
+            End If
+        End If
+
+        'To extract seat number for 10
+        If SeatNo.Length = 3 Then
+            first_index = 10
+            second_index = SeatNo(2)
+
+            'To extract seat number for 1 - 9
+        Else
+            first_index = Val(SeatNo(0))
+            second_index = SeatNo(1)
+        End If
+
+        'To assign column number from seat letter
+        If second_index = "A" Then
+            column = 1
+        End If
+        If second_index = "B" Then
+            column = 2
+        End If
+        If second_index = "C" Then
+            column = 3
+        End If
+        If second_index = "D" Then
+            column = 4
+        End If
+
+        'To check if seat is already assigned by another passenger
+        If aryintPassengers(first_index, column) = "X" Then
+            MsgBox("Seat already assigned!")
+        Else
+            aryintPassengers(first_index, column) = "X"
+            aryPassengerName(first_index, column) = txtBoxName.Text.ToUpper
+            isEmpty = True
+        End If
+
+        'To assign passenger to next available seat if no seat is there
+        If isEmpty = False Then
+            Call addWaitlist()
+        End If
+    End Sub
+
+    Private Sub addWaitlist()
+        'Sub function to add name to the waitlist if there are no empty seats
+        lstBoxWaitlist.Items.Add(aryWaitlistName.Count + 1 & ":" & txtBoxName.Text)
+        aryWaitlistName.Add(txtBoxName.Text)
+
+    End Sub
+
+    Private Function countPassengers() As Integer
+        'Function to count the number of passengers
+        Dim i, j As Integer
+        Dim passengerCount As Integer = 0
+
+        For i = 1 To 10
+            For j = 1 To 4
+                If aryintPassengers(i, j) = "X" Then
+                    passengerCount += 1
+                End If
+            Next
+        Next
+        Return passengerCount
+    End Function
+
+    Private Sub btnAddPassenger_Click(sender As Object, e As EventArgs) Handles btnAddPassenger.Click
+        'Sub function to add a new passenger to the seating chart
+        If IsNumeric(txtBoxName.Text) Then 'input validation
+            MsgBox("Invalid passenger name! Must be characters.")
+            Exit Sub
+        End If
+        If txtBoxName.Text = "" Then
+            MsgBox("Enter passenger name!")
+            Exit Sub
+        End If
+
+        ' Call createArray()
+        Call addPassenger()
+        lstSeatChart.Items.Clear()
+        'txtBoxSeatChart.Clear()
+        Call createArray()
+    End Sub
+
+    Private Sub createArray()
+        'Sub function to create the new array
+        aryintPassengers(0, 0) = "Row"
+        aryintPassengers(0, 1) = "A"
+        aryintPassengers(0, 2) = "B"
+        aryintPassengers(0, 3) = "C"
+        aryintPassengers(0, 4) = "D"
+
+        'To store 1 - 10
+        For i = 1 To 10
+            aryintPassengers(i, 0) = "    " & CStr(i) & " "
+        Next
+
+        'To get effective white spaces
+        For i = 0 To 11
+            Dim output As String = ""
+            For j = 0 To 5
+                output += aryintPassengers(i, j) + "       "
+            Next
+            lstSeatChart.Items.Add(output)
+            'txtBoxSeatChart.Text = output
+        Next
+
+
+    End Sub
+    Private Sub btnDeletePassenger_Click(sender As Object, e As EventArgs) Handles btnDeletePassenger.Click
+        'Sub function to remove a passenger from the seating chart and add the next person on the waiting list
+        Dim i, j As Integer
+        Dim flag As Boolean = False
+
+        If IsNumeric(txtBoxName.Text) Then
+            MsgBox("Invalid passenger name! Must be characters.")
+        End If
+        If txtBoxName.Text = "" Then
+            MsgBox("Enter passenger name!")
+        End If
+
+        For i = 1 To 10
+            For j = 1 To 4
+                If aryPassengerName(i, j) = txtBoxName.Text.ToUpper Then
+                    aryPassengerName(i, j) = ""
+                    aryintPassengers(i, j) = ""
+                    flag = True
+                    lblPassengerCount.Text = "Passenger count: " + CStr(countPassengers())
+
+                    'To remove the first element from the waitlist array
+                    If aryWaitlistName.Count <> 0 Then
+                        aryPassengerName(i, j) = aryWaitlistName(0)
+                        aryintPassengers(i, j) = "X"
+                        aryWaitlistName.RemoveAt(0)
+                        lstBoxWaitlist.Items.RemoveAt(0)
+                        lblPassengerCount.Text = "Passenger count: " + CStr(countPassengers())
+                    End If
+                End If
+            Next
+        Next
+
+        If flag = False Then
+            MsgBox("Passenger not found!")
+            Exit Sub
+        End If
+
+        lstSeatChart.Items.Clear()
+        Call createArray()
+
+    End Sub
+
+    Private Sub btnListPassenger_Click(sender As Object, e As EventArgs) Handles btnListPassenger.Click
+        'Sub function to display the new form of passengers with their seating chart
+        Dim frmDisp As New frmDisplay
+        frmDisp.txtDocument.SelectionStart = 0
+        frmDisp.txtDocument.Text = "#:    Passanger Name:       Seat:" & vbCrLf
+        Dim count As Integer = 1
+
+        For i = 1 To 10
+            For j = 1 To 4
+                If aryintPassengers(i, j) <> "X" Then
+                    aryPassengerName(i, j) = "  *** Available ***"
+                End If
+                frmDisp.txtDocument.Text += count & "      " & aryPassengerName(i, j) & "      " & i & aryintPassengers(0, j) & vbCrLf
+                count += 1
+            Next
+        Next
+        frmDisp.ShowDialog()
+    End Sub
+
+    Private Sub btnResetAll_Click(sender As Object, e As EventArgs) Handles btnResetAll.Click
+        'Sub function to reset the seating chart
+        txtBoxName.Text = ""
+        txtBoxSeat.Text = ""
+        lstBoxWaitlist.Items.Clear()
+
+        For i = 1 To 10
+            For j = 1 To 4
+                If aryintPassengers(i, j) = "X" Then
+                    aryintPassengers(i, j) = ""
+                    aryPassengerName(i, j) = ""
+                End If
+            Next
+        Next
+
+        For i = 0 To aryWaitlistName.Count - 1
+            aryWaitlistName(i) = ""
+        Next
+
+        lblPassengerCount.Text = "Passenger count: " + CStr(countPassengers())
+
+        lstSeatChart.Items.Clear()
+        Call createArray()
+    End Sub
+
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        'Sub function to close the form
+        Me.Close()
+    End Sub
+End Class
